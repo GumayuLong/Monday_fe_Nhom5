cb481dd0242b6d5a2630bf18755a37f38b36aae7
GumayuLong <tothanhlong2002@gmail.com>
2023-12-28 09:21:31 +0700

    Done create account page

--- src/pages/Welcome/CreateAccount.jsx
+++ src/pages/Welcome/CreateAccount.jsx
@@ -18,121 +18,136 @@ export default function CreateAccount() {
 					width: "60%",
 					display: "flex",
 					alignItems: "flex-end",
-                    justifyContent: "space-between",
+					justifyContent: "space-between",
 					flexDirection: "column",
 					height: "100%",
 					padding: "64px 128px",
 				}}
 			>
-				<div style={{ marginBottom: "64px", width: "100%", flex: "1" }}>
+				<div style={{ width: "100%" }}>
 					<img
 						src="https://cdn.monday.com/images/logos/logo-full-big.png"
 						alt=""
-						style={{ height: "24px" }}
+						style={{ height: "24px", marginBottom: "64px" }}
 					/>
-				</div>
-				<div
-					style={{
-						margin: "0 auto",
-						width: "100%",
-						display: "flex",
-						flexDirection: "column",
-						justifyContent: "center",
-                        flex: "11"
-					}}
-				>
-					<div>
-						<h1
-							style={{
-								fontSize: "24px",
-								fontWeight: "500",
-								marginBottom: "24px",
-							}}
-						>
-							Create your account
-						</h1>
-					</div>
-					<div style={{display: "block"}}>
-						<div
-							style={{
-								display: "flex",
-								flexDirection: "column",
-								marginBottom: "20px",
-							}}
-						>
-							<label
-								htmlFor=""
-								style={{ fontSize: "14px", padding: "0 0 4px" }}
-							>
-								Full name
-							</label>
-							<input
-								className="custom-placeholder"
-								type="text"
-								placeholder="Enter your full name"
+					<div
+						style={{
+							margin: "0 auto",
+							width: "100%",
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "center",
+						}}
+					>
+						<div>
+							<h1
 								style={{
-									border: "1px solid #c3c6d4",
-									borderRadius: "5px",
-									width: "75%",
-									height: "48px",
+									fontSize: "24px",
+									fontWeight: "500",
+									marginBottom: "24px",
 								}}
-							/>
-						</div>
-						<div
-							style={{
-								display: "flex",
-								flexDirection: "column",
-								marginBottom: "20px",
-							}}
-						>
-							<label
-								htmlFor=""
-								style={{ fontSize: "14px", padding: "0 0 4px" }}
 							>
-								Password
-							</label>
-							<input
-								className="custom-placeholder"
-								type="password"
-								placeholder="Enter at least 8 characters"
-								style={{
-									border: "1px solid #c3c6d4",
-									borderRadius: "5px",
-									width: "75%",
-									height: "48px",
-								}}
-							/>
-						</div>
-						<div
-							style={{ display: "flex", flexDirection: "column" }}
-						>
-							<label
-								htmlFor=""
-								style={{ fontSize: "14px", padding: "0 0 4px" }}
-							>
-								Account name
-							</label>
-							<input
-								className="custom-placeholder"
-								type="password"
-								placeholder="For example, company's or department's name"
-								style={{
-									border: "1px solid #c3c6d4",
-									borderRadius: "5px",
-									width: "75%",
-									height: "48px",
-								}}
-							/>
+								Create your account
+							</h1>
+							<div>
+								<div
+									style={{
+										display: "flex",
+										flexDirection: "column",
+										marginBottom: "20px",
+									}}
+								>
+									<label
+										htmlFor=""
+										style={{
+											fontSize: "14px",
+											padding: "0 0 4px",
+										}}
+									>
+										Full name
+									</label>
+									<input
+										className="custom-placeholder"
+										type="text"
+										placeholder="Enter your full name"
+										style={{
+											border: "1px solid #c3c6d4",
+											borderRadius: "5px",
+											width: "75%",
+											height: "48px",
+										}}
+									/>
+								</div>
+								<div
+									style={{
+										display: "flex",
+										flexDirection: "column",
+										marginBottom: "20px",
+									}}
+								>
+									<label
+										htmlFor=""
+										style={{
+											fontSize: "14px",
+											padding: "0 0 4px",
+										}}
+									>
+										Password
+									</label>
+									<input
+										className="custom-placeholder"
+										type="password"
+										placeholder="Enter at least 8 characters"
+										style={{
+											border: "1px solid #c3c6d4",
+											borderRadius: "5px",
+											width: "75%",
+											height: "48px",
+										}}
+									/>
+								</div>
+								<div
+									style={{
+										display: "flex",
+										flexDirection: "column",
+									}}
+								>
+									<label
+										htmlFor=""
+										style={{
+											fontSize: "14px",
+											padding: "0 0 4px",
+										}}
+									>
+										Account name
+									</label>
+									<input
+										className="custom-placeholder"
+										type="password"
+										placeholder="For example, company's or department's name"
+										style={{
+											border: "1px solid #c3c6d4",
+											borderRadius: "5px",
+											width: "75%",
+											height: "48px",
+										}}
+									/>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
 				<div
 					style={{
 						alignItems: "center",
-						marginBottom: "64px",
 					}}
 				>
-					<button className="button-continue" style={{width: "100%"}}>Continue</button>
+					<button
+						className="button-continue"
+						style={{ width: "100%" }}
+					>
+						Continue
+					</button>
 				</div>
 			</div>
 			<div style={{ width: "40%" }}>
